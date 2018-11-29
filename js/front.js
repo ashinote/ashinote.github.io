69ba30095c1c771cb90f3cc41e25ef9d4c0db7d1
Sosuke Kimura at Circle CI <sosuke.kimura@ashinote.com>
2018-11-29 03:19:21 +0000

    Update blog via Circle CI

--- js/front.js
+++ js/front.js
@@ -127,23 +127,23 @@ function sliders () {
       lazyLoad: true
     })
 
-    $('.homepage').owlCarousel({
-      navigation: false, // Show next and prev buttons
-      navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 2000,
-      paginationSpeed: 1000,
-      autoPlay: true,
-      stopOnHover: true,
-      singleItem: true,
-      lazyLoad: false,
-      addClassActive: true,
-      afterInit: function () {
-        // animationsSlider()
-      },
-      afterMove: function () {
-        // animationsSlider()
-      }
-    })
+    // $('.homepage').owlCarousel({
+    //   navigation: false, // Show next and prev buttons
+    //   navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    //   slideSpeed: 2000,
+    //   paginationSpeed: 1000,
+    //   autoPlay: true,
+    //   stopOnHover: true,
+    //   singleItem: true,
+    //   lazyLoad: false,
+    //   addClassActive: true,
+    //   afterInit: function () {
+    //     // animationsSlider()
+    //   },
+    //   afterMove: function () {
+    //     // animationsSlider()
+    //   }
+    // })
   }
 }
 
